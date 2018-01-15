1230d2f0fcedca38abc626e80224d7eebce7837b
geoffrey woodcock <theatomproject@gmail.com>
2018-01-15 20:13:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = new Object({})
 var obj = {prop: 1}
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({},obj,{[key]: value})
-  
+
 }
 function updateObjectWithKeyAndValue(object, key, value){
   object[key]= value
