1c5cf0ad99e78e4f91803908a206bd2dfa794599
geoffrey woodcock <theatomproject@gmail.com>
2018-01-15 20:27:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function deleteFromObjectByKey(object, key){
   return obj
 }
 function destructivelyDeleteFromObjectByKey(object, key){
-  delete object.key;
-  return object
+  delete obj.key;
+  return obj
 }
