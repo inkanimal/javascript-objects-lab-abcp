da51a0b9de0f55c44a2210b70f156eb86dd9e5f6
geoffrey woodcock <theatomproject@gmail.com>
2018-01-15 20:29:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function deleteFromObjectByKey(object, key){
-  var newObj = Object.assign({}, obj)
-  delete newObj.key;
-  return obj
+  var newObj = Object.assign({}, object)
+  delete newObj[key];
+  return newObj
 }
 function destructivelyDeleteFromObjectByKey(object, key){
   delete object.key;
